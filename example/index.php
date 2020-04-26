e807d9a6a50c8b186b96ae882d1dd9d2d33be0c3
Silvio Delgado <3129492+silviodelgado@users.noreply.github.com>
2020-04-26 14:55:45 -0300

    Update index.php

--- example/index.php
+++ example/index.php
@@ -5,6 +5,6 @@ require '../vendor/autoload.php';
 define('MD_NAME', 'Markdown Wiki - User Guide');
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_CONTENTS', dirname(__DIR__) . DS . 'contents' . DS);
+define('ROOT_CONTENTS', dirname(dirname(__DIR__)) . DS . 'contents' . DS);
 
 \Interart\MarkdownWiki\MarkdownWiki::init(ROOT_CONTENTS);
